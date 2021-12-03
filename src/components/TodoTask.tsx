58f2deaad01f18c05cca91fc5f424e6308c1d991
MeryAmun <chebesiawah@gmail.com>
2021-12-03 23:27:25 +0100

    styles added

--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -4,12 +4,14 @@ import { Tasks } from '../Types'
 type TaskProps = {
   task: Tasks
 }
-export const TodoTask = (props: TaskProps) => {
+export const TodoTask = ({ task }: TaskProps) => {
   return (
-    <div>
-      <h2>Tasks</h2>
-      <p>{props.task.taskName}</p>
-      <p>{props.task.deadline}</p>
+    <div className='task'>
+      <div className='content'>
+        <span>{task.taskName}</span>
+        <span>{task.deadline}</span>
+      </div>
+      <button>X</button>
     </div>
   )
 }
